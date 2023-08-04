af52fa1efd6ef1154d9f592bd7323e2c64ad4beb
grittypuffy <grittypuffy@riseup.net>
2023-08-04 13:46:48 +0530

    Update: analysis_options, test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'wifi/configure_settings.dart';
 
+// TODO(grittypuffy): Refactor codebase
 class App extends StatelessWidget {
   const App({super.key});
   @override
@@ -13,18 +14,18 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'NetMan',
-     theme : ThemeData(
-          primarySwatch: Colors.lightBlue,
-          brightness: Brightness.dark,
-          useMaterial3: true,
-        ),
-     themeMode: ThemeMode.dark,
-     debugShowCheckedModeBanner: false,
-     home: const ConfigureWiFi(),
-   );
- }
+    return MaterialApp(
+      title: 'NetMan',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        brightness: Brightness.dark,
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      home: const ConfigureWiFi(),
+    );
+  }
 }
 
 void main() => runApp(const App());
--- lib/wifi/widget/qr_generate.dart
+++ lib/wifi/widget/qr_generate.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'ssid_widget.dart';
 
 class QRGenerate extends StatefulWidget {
   const QRGenerate({super.key});
@@ -11,10 +10,10 @@ class _QRGenerateState extends State<QRGenerate> {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: (){
+      onPressed: () {
         showDialog(
           context: context,
-          builder: (BuildContext context){
+          builder: (BuildContext context) {
             return AlertDialog(
               title: const Text('Wi-fi Credentials'),
               content: const Column(
@@ -24,22 +23,22 @@ class _QRGenerateState extends State<QRGenerate> {
                   Text('SSID: Home-Wifi'),
                   Text('WPA-2 PSK: 63GDieb^27!@&hdUWI18'),
                   Image(image: AssetImage('assets/images/qr.png')),
-                  ],
-                ),
-                actions: [
-              TextButton(
-                child: const Text('Close'),
-                onPressed: (){
-                  Navigator.of(context).pop();
-                },
+                ],
               ),
-            ],
-              );
-            },
-          );
-        },
-  backgroundColor: Colors.deepPurple,
-  child: const Icon(Icons.qr_code_2),
+              actions: [
+                TextButton(
+                  child: const Text('Close'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+      backgroundColor: Colors.deepPurple,
+      child: const Icon(Icons.qr_code_2),
     );
   }
 }
