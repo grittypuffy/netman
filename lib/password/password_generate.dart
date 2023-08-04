c170b67a4882c195bd4e292a87f34f691fa26c0d
grittypuffy <grittypuffy@riseup.net>
2023-08-04 13:26:07 +0530

    Modified password_generate

--- lib/password/password_generate.dart
+++ lib/password/password_generate.dart
@@ -1,17 +1,17 @@
 import 'dart:math';
 
-void main(){
-  passwordGenerate(20);
+void main() {
+  passwordGenerate(length: 20);
 }
 
-String passwordGenerate(int length){
+String passwordGenerate({int length = 20}) {
   const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
   const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const numbers = '0123456789';
   const symbols = '!@#\$%^&*()_+-={}[]:;<,>./?\'"~`|"';
-  String characters = '$lowerCase$upperCase$numbers$symbols';
-  return List.generate(length, (index){
-      final random = Random.secure().nextInt(characters.length);
-      return characters[random];
+  const characters = '$lowerCase$upperCase$numbers$symbols';
+  return List.generate(length, (index) {
+    final random = Random.secure().nextInt(characters.length);
+    return characters[random];
   }).join('');
 }
